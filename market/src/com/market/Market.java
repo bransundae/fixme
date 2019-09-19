a12357356c7f3fb460e087000ccc126f286a244f
brandon JOHNSON <bjohnso@c3r3s3.wethinkcode.co.za>
2019-09-19 17:33:08 +0200

    began building router communication capabilities

--- market/src/com/market/Market.java
+++ market/src/com/market/Market.java
@@ -1,4 +1,14 @@
 package com.market;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Market {
+
+    public static void Market(String args[]) throws IOException {
+        Socket socket = new Socket("localhost", 5001);
+
+
+    }
+
 }
