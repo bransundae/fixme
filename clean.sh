61151367cd863cecacb5645bc000a0948345feb4
bjohnso <bjohnso@student.wethinkcode.co.za>
2019-09-19 14:40:18 +0200

    Created Build and Execution Scripts for Simple Testing as Well as Basic Server-Client Functionality

---
+++ clean.sh
@@ -0,0 +1,4 @@
+[ -d build ] && rm -rf build
+[ -f simulation.txt ] && rm -rf simulation.txt
+rm -rf swingydb
+rm -rf target
--- src/com/fixme/Router.java
+++ src/com/fixme/Router.java
@@ -1,4 +1,27 @@
 package com.fixme;
 
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Router {
+
+    public static void main(String args[]) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(5000);
+        Socket socket = serverSocket.accept();
+
+        System.out.println("Broker Connected");
+
+        InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String string = bufferedReader.readLine();
+        System.out.println("Broker: " + string);
+
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+        printWriter.println("10 apples have been sent");
+        printWriter.flush();
+    }
+
 }
